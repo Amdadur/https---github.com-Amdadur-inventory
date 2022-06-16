a958683eb966cff1c9384848fb6f0bbcfeeeda3d
Amdadur <amdad@live.co.uk>
2022-06-16 03:42:27 +0100

    dates added

--- script.js
+++ script.js
@@ -1,8 +1,178 @@
+const shelf_A1 = [
+    ["Wine Gums", "28 AUG 22", "28 APR 23"],
+    ["Jelly Babies (Dusted)", "28 JUL 22"],
+    ["Rhubarb & Custard", "29 MAR 24"],
+    ["Beer Bottles", "28 MAR 23", "28 JUN 23"],
+    ["Fruit Pastilles", "28 NOV 22", "28 DEC 22"],
+    ["Rosie Apples", "28 FEB 24"],
+    ["Love Hearts", "28 SEP 22", "31 MAY 23"],
+];
 
-const shelf_A1 = {
-    ["Wine Gums"]
-    []
-    []
-    []
-    []
+const shelf_A2 = [
+    ["Milk Choc Raisins", "28 NOV 22"],
+    ["Catherine Wheels", "28 NOV 22", "28 SEP 23"],
+    ["Snowies", "28 FEB 23"],
+    ["Pineapple Chunks", "07 MAR 24"],
+    ["Rum & Raisin Fudge", "02 DEC 22"],
+    ["Fruity Meerkats", "28 MAR 23"],
+    ["Trad Mint Humbugs", "28 APR 23"],
+    ["Sherbet Strawberries", "28 NOV 22", "28 JAN 23"],
+];
+
+const shelf_A3 = [
+    ["Gummy Bears", "28 MAY 23"],
+    ["Fudge Duo", "27 SEP 22", "11 FEB 23"],
+    ["Dolly Mixture", "28 OCT 22", "28 DEC 22"],
+    ["Luxury Fruit Jellies", "28 OCT 22", "28 DEC 22"],
+    ["Fried Eggs", "28 FEB 23"],
+    ["Dolly Mixture", "28 JUN 22", "28 OCT 22", "28 DEC 22"],
+];
+
+const shelf_B1 = [
+    ["Milk Chocolate Beans", "28 OCT 22"],
+    ["Milk Chocolate Raisins", "28 JUL 22"],
+    ["Lemon Bonbons", "28 NOV 22"],
+    ["Mini Eggs", "10 MAY 23"],
+    ["Mint Imperials", "28 JUL 22", "22 JAN 23"],
+    ["Pear Drops", "22 MAR 23", "06 OCT 23"],
+    ["Pineapple Chunks", "20 APR 23", "07 MAR 24"],
+];
+
+const shelf_B2 = [
+    ["Rosie Apples", "22 FEB 24", "28 FEB 24"],
+    ["Sherbet Strawberries", "28 JAN 23"],
+    ["Strawberry Bonbons", "18 MAY 23"],
+    ["Rhubarb & Custard", "14 AUG 23", "28 AUG 23", "22 SEP 23", "21 DEC 23"],
+    ["Sherbet Lemon", "28 JUN 22", "28 SEP 22", "28 JAN 23"],
+    ["Snowies", "28 FEB 23"],
+];
+
+const shelf_B3 = [
+    ["Wriggly Worms", "28 MAR 23", "28 APR 23"],
+    ["Yoghurt Coated Raisins", "28 MAR 23"],
+    ["Trad Mint Humbugs", "28 APR 23"],
+    ["Toffee Bonbons", "15 JUL 22", "19 AUG 22", "10 JAN 23"],
+    ["Strawberry Milkshakes", "28 SEP 22", "28 MAY 23"],
+    ["Giant Strawberries", "28 AUG 22", "28 DEC 22", "28 MAR 23", "28 APR 23"],
+];
+
+const shelf_C1 = [
+    ["Fruit Pastilles", "28 NOV 22", "28 OCT 22", "28 DEC 22"][
+        ("Fruity Fangs", "28 JAN 23", "28 MAR 23")
+    ][("Fruity Meerkats", "28 SEP 22", "28 NOV 22", "28 MAR 23")][
+        ("Gummy Bears", "28 JAN 23", "28 MAY 23")
+    ][("Jazzies", "28 OCT 22", "28 JAN 23")][
+        ("Jelly Babies (Dusted)", "28 JUN 22")
+    ],
+];
+
+const shelf_C2 = [
+    ["Jelly Beans", "04 DEC 22", "01 DEC 23", "16 DEC 23"][
+        ("Jelly Buttons", "28 OCT 22")
+    ][("Jelly Rings", "28 APR 23", "28 MAY 23")][
+        ("Kola Cubes", "23 SEP 23", "02 FEB 24")
+    ][("Lemon Bonbons", "17 AUG 22", "20 APR 23")][
+        ("Liquorice Allsorts", "28 OCT 22")
+    ],
+];
+
+const shelf_C3 = [
+    ["Liquorice Comfits", "28 JAN 23", "28 APR 23"][
+        ("Liquorice Cream Rock", "01 JUN 23")
+    ][("Love Hearts", "28 SEP 22", "31 JAN 23", "31 MAR 23")][
+        ("Luxury Fruit Chews", "28 JAN 23", "28 MAR 23", "28 OCT 23")
+    ][("Luxury Fruit Jellies", "28 JUL 22", "28 SEP 22", "28 OCT 22")][
+        ("Milk Bottles", "28 NOV 22", "28 DEC 22", "28 JAN 23")
+    ],
+];
+
+const shelf_D1 = [
+    ["ABC's", "28 JAN 23", "30 SEP 23"][
+        ("American Hard Gums", "28 OCT 22", "28 JAN 23", "28 FEB 23")
+    ][("Aniseed Balls", "07 DEC 23", "17 MAY 24", "23 AUG 24")][
+        ("Beer Bottles", "28 MAR 23", "28 JUN 23")
+    ][("Black Jacks & Fruit Salad", "28 SEP 22")][
+        ("Bubblegum Bonbons", "18 MAY 23")
+    ],
+];
+
+const shelf_D2 = [
+    ["Bubblegum Bottles", "28 AUG 22", "28 MAY 23"][
+        ("Candy Necklaces", "28 JUN 23")
+    ][("Catherine Wheels", "28 NOV 22", "28 SEP 23")][
+        ("Fudge Duo", "27 SEP 22", "11 FEB 23")
+    ][("Clotted Cream Fudge", "26 JAN 23", "04 FEB 23")][
+        ("Rum & Raisin Fudge", "29 JUL 22", "11 OCT 22", "02 DEC 22")
+    ],
+];
+
+const shelf_D3 = [
+    ["Fizzy Cola Bottles", "28 JUL 22", "28 SEP 22", "28 DEC 22", "28 JUL 23"][
+        ("Fizzy Dummies", "28 AUG 22", "28 FEB 23", "28 APR 23")
+    ][("Fizzy Fun Mix", "28 JUL 22", "28 SEP 23", "28 FEB 23")][
+        ("Fizzy Strawberries", "28 SEP 22", "28 MAR 23", "28 JUN 23")
+    ][("Fruit Pips", "28 SEP 22", "28 JAN 23")][("Fried Eggs", "28 FEB 23")],
+];
+
+const shelf_E1 = [
+    ["Jelly Rings", "28 MAY 23"][("Fizzy Cola Bottles", "28 JUL 23")][
+        ("Candy Necklaces", "28 OCT 23")
+    ][("Wiggly Worms", "28 MAY 23")][("Liquorice Allsorts", "28 OCT 22")][
+        ("Mini Eggs", "21 JUL 23")
+    ],
+];
+
+const shelf_E2 = [
+    ["Random Retros", "28 FEB 23", "30 APR 23"][
+        ("Chocolate Footballs", "28 SEP 22")
+    ][("Strawberry Bonbons", "28 MAY 23")][("American Hard Gums", "28 FEB 23")][
+        ("Bubblegum Bottles", "28 MAY 23")
+    ][("Milk Chocolate Beans", "28 NOV 22")],
+];
+
+const shelf_E3 = [
+    ["Kola Cubes", "10 MAR 24"][("Fruity Meerkats", "28 MAR 23", "28 MAY 23")][
+        ("Liquorice Cream Rock", "01 JUN 23")
+    ][("Beer Bottles", "28 JUN 23")][("Catherine Wheels", "28 FEB 24")][
+        ("Jelly Beans", "30 JAN 24")
+    ],
+];
+
+const shelf_F1 = [
+    ["Fruity Fangs", "28 MAY 23"][("Fizzy Fun Mix", "28 SEP 22")][
+        ("Clotted Cream Fudge", "01 FEB 23")
+    ][("Snowies", "28 FEB 23")][("Fizzy Strawberries", "28 JUN 23")][
+        ("Fruit Pastilles", "28 MAY 22", "28 NOV 22", "28 DEC 22", "28 JAN 23")
+    ],
+];
+
+const shelf_F2 = [
+    ["Rhubarb & Custard", "21 DEC 23", "29 MAR 24"][
+        ("Dolly Mixture", "28 DEC 22")
+    ][("Milk Bottles", "28 DEC 22", "28 JAN 23")][
+        ("Beer Bottles", "28 MAR 23")
+    ][("Toffee Bonbons", "21 APR 23")][("Aniseed Balls", "06 DEC 24")][
+        ("Pear Drops", "28 APR 24")
+    ][("Milk Chocolate Raisins", "28 FEB 23")],
+];
+
+const shelf_F3 = [
+    ["Sherbet Strawberries", "28 NOV 22", "28 JAN 23"][
+        ("Gummy Bears", "28 MAY 23")
+    ][("Luxury Fruit Chews", "28 OCT 23")][
+        ("Lemon Bonbons", "20 APR 23", "04 MAY 23")
+    ][("Wine Gums", "28 AUG 22", "28 APR 23")],
+];
+
+function create() {
+    const a1 = document.querySelector(".aone");
+
+    for (var i = 1; i < shelf_A1.length; i++) {
+        var eachTip = document.createElement("div");
+        eachTip.setAttribute("id", "newD");
+        eachTip.innerHTML = `<h1>${shelf_A1[i][0]}</h1><p>${shelf_A1[i][1]}</p>`;
+        a1.appendChild(eachTip);
+    }
 }
+
+create();
